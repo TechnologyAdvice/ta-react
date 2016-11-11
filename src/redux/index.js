5ad51011b79bd41431f410dea0283d32df31c291
Levi Thomason <me@levithomason.com>
2016-11-11 14:51:33 -0800

    fix(redux): add createModule to index

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,4 @@
 export { default as createConstants } from './create-constants'
+export { default as createModule } from './create-module'
 export { default as createReducer } from './create-reducer'
 export { default as createStore } from './create-store'
